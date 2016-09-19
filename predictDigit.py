867f8a0c8ff314c2882804cfe78a049f37173bc6
Enio <sfmneo@gmail.com>
2016-09-19 20:14:53 +0000

    softmax

--- predictDigit.py
+++ predictDigit.py
@@ -17,7 +17,7 @@ saver = tf.train.Saver()
 
 with tf.Session() as sess:
     sess.run(init)
-    saver.restore(sess, "model.ckpt")
+    saver.restore(sess, "softmax.ckpt")
   
     prediction=tf.argmax(y,1)
     #first value in list
