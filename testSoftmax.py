7554bd9363515ea4695760784e3c2dda243cbe26
Enio <sfmneo@gmail.com>
2016-09-19 19:36:38 +0000

    updated tests

--- testSoftmax.py
+++ testSoftmax.py
@@ -8,9 +8,6 @@ b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder(tf.float32, [None, 10])
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
 init = tf.initialize_all_variables()
 saver = tf.train.Saver()
 
