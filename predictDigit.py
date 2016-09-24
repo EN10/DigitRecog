8c1063b6be4b811d9c754694e7264ee93707a43b
Enio <sfmneo@gmail.com>
2016-09-24 10:53:20 +0000

    update model

--- predictDigit.py
+++ predictDigit.py
@@ -7,9 +7,13 @@ digit = json.loads(JSON)
 
 import tensorflow as tf
 x = tf.placeholder(tf.float32, [None, 784])
-W = tf.Variable(tf.zeros([784, 10]))
+N = 100
+w2 = tf.Variable(tf.truncated_normal([784, N], stddev=0.1))
+b2 = tf.Variable(tf.zeros([N]))
+y2 = tf.nn.relu(tf.matmul(x, w2) + b2)
+w = tf.Variable(tf.truncated_normal([N, 10], stddev=0.1))
 b = tf.Variable(tf.zeros([10]))
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+y = tf.nn.softmax(tf.matmul(y2, w) + b)
 y_ = tf.placeholder(tf.float32, [None, 10])
 
 init = tf.initialize_all_variables()
@@ -17,7 +21,7 @@ saver = tf.train.Saver()
 
 with tf.Session() as sess:
     sess.run(init)
-    saver.restore(sess, "softmax.ckpt")
+    saver.restore(sess, "2LModel.ckpt")
   
     prediction=tf.argmax(y,1)
     #first value in list
